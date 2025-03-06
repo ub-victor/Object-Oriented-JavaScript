ac83b6c3cc547c485f321730d64b3db2c7aed058
ub-victor <victoireushindi371@gmail.com>
2025-03-06 10:58:58 +0200

    Intial Commit

--- objects.js
+++ objects.js
@@ -13,6 +13,6 @@ var userOneFriends = ['Ryu', 'Mario'];
 // Create user three
 
 var userOneEmail = 'ryu@Constant.com';
-var userOneName = 'Yoshi';
-var userOneFriends = ['Ryu', 'Mario'];
+var userOneName = 'Mario';
+var userOneFriends = ['Yoshi'];
 
