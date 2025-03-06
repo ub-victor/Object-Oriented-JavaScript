313477f395a3c627fc7aa467af02cc1e7a2cbeea
ub-victor <victoireushindi371@gmail.com>
2025-03-06 11:22:26 +0200

    Intial Commit

--- objects.js
+++ objects.js
@@ -9,34 +9,8 @@ var userOne ={
     logout(){
         console.log(this.email, 'has logged out');
     }
-}
+};
 
-// Create user two
+console.log(userOne.friends);
 
-var userOneEmail = 'ryu@victorine.com';
-var userOneName = 'Yoshi';
-var userOneFriends = ['Ryu', 'Mario'];
-
-// Create user three
-
-var userOneEmail = 'ryu@Constant.com';
-var userOneName = 'Mario';
-var userOneFriends = ['Yoshi'];
-
-function login(email){
-    console.log(email, "is now online")
-}
-
-function logout(email){
-    console.log(email, "has logged out")
-}
-
-
-function logFriends(friends){
-    friends.forEach(friends =>{
-        console.log(friends)
-    });
-}
-
-login(userOneEmail);
 
