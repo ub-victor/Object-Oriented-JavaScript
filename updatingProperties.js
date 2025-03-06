1a3b7840349b709856df8807da608743e615ce95
ub-victor <victoireushindi371@gmail.com>
2025-03-06 12:02:46 +0200

    Intial Commit

--- updatingProperties.js
+++ updatingProperties.js
@@ -15,3 +15,46 @@ userOne.name = 'Victor';// Update the name property
 // you can also acess a name and change it var name the name by saying  pro= name, userOne[pro] = 'Victor';
 // to add properties on the object you can say userOne['age'] = 25; or userOne.age = 25;
 userOne.age = 25;
+
+// to add a function to the object you can say userOne.info = function(){console.log('this is a function')};
+
+// Create user two
+
+var userTwo ={  
+    email: 'ryu@victorine.com',  
+    name: 'Yoshi',  
+    friends: ['Ryu', 'Mario'],  
+    login(){  
+        console.log(this.email, 'has logged in');  
+    },  
+    logout(){  
+        console.log(this.email, 'has logged out');  
+    }  
+};
+
+var userOneEmail = 'ryu@victorine.com';
+var userOneName = 'Yoshi';
+var userOneFriends = ['Ryu', 'Mario'];
+
+// Create user three
+
+var userOneEmail = 'ryu@Constant.com';
+var userOneName = 'Mario';
+var userOneFriends = ['Yoshi'];
+
+function login(email){
+    console.log(email, "is now online")
+}
+
+function logout(email){
+    console.log(email, "has logged out")
+}
+
+
+function logFriends(friends){
+    friends.forEach(friends =>{
+        console.log(friends)
+    });
+}
+
+login(userOneEmail);
