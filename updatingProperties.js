3b4aabda27ace639b54cff435c9488df22f9e0fd
ub-victor <victoireushindi371@gmail.com>
2025-03-06 11:40:04 +0200

    Intial Commit

---
+++ updatingProperties.js
@@ -0,0 +1,13 @@
+// Create user one  
+var userOne ={  
+    email: 'ryu@victor.com',  
+    name: 'Ryu',  
+    friends: 'Yoshi',  
+    login(){  
+        console.log(this.email, 'has logged in');  
+    },  
+    logout(){  
+        console.log(this.email, 'has logged out');  
+    }  
+};  
+
