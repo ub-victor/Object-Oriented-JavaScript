5246984f7232c9ab350ce68a7b1673b80946ea8e
ub-victor <victoireushindi371@gmail.com>
2025-03-09 11:17:38 +0200

    Intial Commit

--- methodChaining.js
+++ methodChaining.js
@@ -28,3 +28,4 @@ var userS = new User('ryu@victorine.com', 'Victorine');
 // Method chaining: calling multiple methods on the same object in a single statement
 userF.login().updatedScore().updatedScore().logout();
 
+
