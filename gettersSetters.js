1bb681c16e64cf29855f5cc257212adeb925a46f
ub-victor <victoireushindi371@gmail.com>
2025-03-10 12:23:26 +0200

    Intial Commit

--- gettersSetters.js
+++ gettersSetters.js
@@ -8,6 +8,9 @@ class Square {
     // The 'get' keyword is used to define a getter method for the 'area' property.
     // This allows us to access the area of the square as a property, rather than calling a method.
     // It is placed here to provide a convenient way to calculate the area based on the current width and height.
+    /*get area(){ ... }
+
+    This defines a "getter" method called area. The get keyword is crucial here. It allows you to access the calculated area as if it were a property of the object, rather than a method you have to call with parentheses. */
     get area(){
         return this.width * this.height;
     }
