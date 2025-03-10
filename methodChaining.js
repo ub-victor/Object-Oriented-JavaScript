ad82684c0973dbde83fb53aabea55611d04f068b
ub-victor <victoireushindi371@gmail.com>
2025-03-11 01:27:20 +0200

    Intial Commit

--- methodChaining.js
+++ methodChaining.js
@@ -27,4 +27,24 @@ var userS = new Users('ryu@victorine.com', 'Victorine');
 //userOne.login(); 
 
 // Method chaining: calling multiple methods on the same object in a single statement
+
+
+/**  
+ * The 'return this;' statement in each method is essential for enabling   
+ * method chaining in this class. By returning the current instance of   
+ * the object (this), we allow multiple method calls to be chained   
+ * together in a single statement. This enhances code readability and   
+ * maintainability, as it allows the developer to perform a series of   
+ * actions on an object in a concise manner.   
+ * Without 'return this;', the chained methods would operate on   
+ * 'undefined', resulting in errors and breaking the intended flow   
+ * of execution.  
+ */  
+
 userF.login().updatedScore().updatedScore().logout();
+
+
+/* Why the User Instance Isn’t Outputted
+Behavior of the Methods:
+
+Each method logs a specific message to the console when it’s called. However, you haven’t included a console.log() for the instance itself in methods like login() or logout(). As a result, the output will only reflect the specific messages contained within those methods, not the entire user instance.*/
