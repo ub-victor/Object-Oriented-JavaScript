47bc57f42110b8dfe94ce122a59aadd66feaedad
ub-victor <victoireushindi371@gmail.com>
2025-03-10 11:03:15 +0200

    Intial Commit

---
+++ es6Classes.js
@@ -0,0 +1,23 @@
+/*
+
+    Class --> Object
+
+    Instance Properties : What they have 
+        -name
+        -age
+        -height
+
+    Instance Methods : What they do
+
+        - tallk
+        - run
+        - jump
+*/
+
+class Rectangle { // Class declaration
+    // it used to setup your Object
+    constructor() { 
+        console.log("Rectangle is created");
+    }
+
+}
