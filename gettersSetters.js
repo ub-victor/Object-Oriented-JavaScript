57b9194a9c4f5ba5c90a7851b455d49421e2abf8
ub-victor <victoireushindi371@gmail.com>
2025-03-10 12:15:26 +0200

    Intial Commit

---
+++ gettersSetters.js
@@ -0,0 +1,16 @@
+class Square {
+    constructor(_width){
+        this.width = _width;
+        this.height = _width;
+
+    }
+
+    // The 'get' keyword is used to define a getter method for the 'area' property.
+    // This allows us to access the area of the square as a property, rather than calling a method.
+    // It is placed here to provide a convenient way to calculate the area based on the current width and height.
+    get area(){
+        return this.width * this.height;
+    }
+}
+
+let square1 = new Square(25);
