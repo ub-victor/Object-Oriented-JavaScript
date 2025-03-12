975973ba7f7d76acd4501909c66364660ddde2f1
ub-victor <victoireushindi371@gmail.com>
2025-03-12 11:01:07 +0200

    Intial Commit

--- classInheritance.js
+++ classInheritance.js
@@ -36,3 +36,6 @@ var Person2 = new Person('ryu@victorine.com', 'Victorine');
 var admin = new Admin("vainqueur@gmail.com", "Vainqueur");
 
 var persons = [Person1, Person2];
+
+admin.deletePerson(Person1);
+console.log(persons);
