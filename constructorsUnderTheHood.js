77c14dd7f0cc4cc38f9c93978a547f5eb3b9d101
ub-victor <victoireushindi371@gmail.com>
2025-03-13 17:31:09 +0200

    Intial Commit

--- constructorsUnderTheHood.js
+++ constructorsUnderTheHood.js
@@ -2,15 +2,18 @@ function People(email, name) {
     this.email = email;  
     this.name = name;  
     this.online = false;  
-    this.logins = function() {  
-        console.log(this.email, 'has logged in');  
+    //this.logins = function() {  
+    //    console.log(this.email, 'has logged in');  
     }  
 }  
 
 // what is a prototype?
 // A prototype is a property of a function that points to an object.
 //eg: People.prototype
-People.prototype
+People.prototype.login = function(){
+    this.online = true;
+    console.log(this.email, 'has logged in');
+}
 
 var p1 = new People('ryu@victor.com', 'Victor');  
 var p2 = new People('ryu@constant.com', 'Constant'); 
