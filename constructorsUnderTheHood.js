626f216e477d1377dbf7f70678f763bf7c79cca6
ub-victor <victoireushindi371@gmail.com>
2025-03-13 16:07:30 +0200

    Intial Commit

---
+++ constructorsUnderTheHood.js
@@ -0,0 +1,8 @@
+function People (email, name){
+    this.email = email;
+    this.name = name;
+    this.online = false;
+}
+
+var p1 = new People('ryu@victor.com', 'Victor');
+var p2 = new People('ryu@victorine.com', 'Victorine');
