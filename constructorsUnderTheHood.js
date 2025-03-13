0d406fbb3272e9fa8af7cb0524b82dab3f4b7431
ub-victor <victoireushindi371@gmail.com>
2025-03-13 15:50:45 +0200

    Intial Commit

---
+++ constructorsUnderTheHood.js
@@ -0,0 +1,20 @@
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this.score = 0;
+    }
+    login() {
+        console.log(this.email, 'just logged in');
+        return this;
+    }
+    logout() {
+        console.log(this.email, 'just logged out');
+        return this;
+    }
+    updated() {
+        this.score++;
+        console.log(this.email, 'score is now', this.score);
+        return this;
+    }
+}
